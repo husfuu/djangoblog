c546f77b6ea7b96842b85cc42b2c744e9176bfb1
husfuu <muhammadhusninurfadillah@gmail.com>
2022-12-31 15:25:47 +0700

    feat(category): add category feature

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,16 @@
 from blog import views
 from django.urls import path
 
-app_name = 'blog'
+app_name = "blog"
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/new', views.post_create, name='post_create'),
-    path('post/<int:pk>/', views.post_detail, name="post_detail"),
-    path('post/<int:pk>/edit', views.post_edit, name="post_edit")
+    path("", views.post_list, name="post_list"),
+    path("post/new", views.post_create, name="post_create"),
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
+    path("post/<int:pk>/edit", views.post_edit, name="post_edit"),
+    path(
+        "post/category/<int:category_id>",
+        views.post_list_by_category,
+        name="post_category",
+    ),
 ]
