3810ca84967a5b8cf54e142d12f212c55d3cefeb
husfuu <muhammadhusninurfadillah@gmail.com>
2023-01-02 11:04:36 +0700

    feat(comment): add comment feature

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Comment
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'published_date']
+    list_display = ["title", "author", "published_date"]
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ["name"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["author", "post"]
